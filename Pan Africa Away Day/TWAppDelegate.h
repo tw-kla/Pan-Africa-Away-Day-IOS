0e35005136b6f6e1e708ab3fa4cec7cd53428060
James Muranga <jmured@gmail.com>
2014-01-02 09:49:38 +0300

    Initial Commit

---
+++ Pan Africa Away Day/TWAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  TWAppDelegate.h
+//  Pan Africa Away Day
+//
+//  Created by James Muranga on 1/2/14.
+//  Copyright (c) 2014 Thoughtworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Pan Africa Away DayTests/Pan_Africa_Away_DayTests.m
@@ -0,0 +1,34 @@
+//
+//  Pan_Africa_Away_DayTests.m
+//  Pan Africa Away DayTests
+//
+//  Created by James Muranga on 1/2/14.
+//  Copyright (c) 2014 Thoughtworks. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Pan_Africa_Away_DayTests : XCTestCase
+
+@end
+
+@implementation Pan_Africa_Away_DayTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
